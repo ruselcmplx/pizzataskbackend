a0fb64a59c34ffd76f9236ec34f65b661bd5fd99
ruselcmplx <razinurov@gmail.com>
2020-04-19 12:39:33 +0300

    disable factories, change column type

--- database/migrations/2020_04_18_112417_create_orders_table.php
+++ database/migrations/2020_04_18_112417_create_orders_table.php
@@ -18,7 +18,7 @@ class CreateOrdersTable extends Migration
             $table->id();
             $table->bigInteger('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
-            $table->json('order_contents');
+            $table->text('order_contents');
             $table->float('total_price');
             $table->timestamps();
         });
